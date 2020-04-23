5752b3e6fcd21b87bc6607a88f6fcf184e6e904a
TShelton <todd@devworkshop.org>
2020-04-23 15:58:38 -0400

    building browse
    Adding content to browse and added listeners

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Recipe Jungle Cook
+
+## This is the most awesome recipe app
+
+This is an example of a recipe app that is a CRUD application
--- public/app/app.js
+++ public/app/app.js
@@ -1,17 +1,25 @@
+function initListeners() {
+  $("#nav-holder nav .links a").click(function (e) {
+    let id = e.currentTarget.id;
+    changeView(id);
+  });
+}
+
 function initApp() {
   $.get("../views/nav/nav.html", function (data) {
     $("#nav-holder").html(data);
+    initListeners();
   });
 
   $.get("../views/footer/footer.html", function (data) {
     $("footer").html(data);
   });
 
-  changeView("home");
+  changeView("browse");
 }
 
 function changeView(viewName) {
-  $.get(`../views/home/${viewName}.html`, function (data) {
+  $.get(`../views/${viewName}/${viewName}.html`, function (data) {
     $("#app").html(data);
   });
 }
